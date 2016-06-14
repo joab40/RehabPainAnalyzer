486df566308fc07f56cda4b7843df586a690ee30
joab40 <tornvagen@gmail.com>
2016-06-14 22:33:52 +0200

    Algorithm descripton for print non empty values

--- modify_csv_file.py
+++ modify_csv_file.py
@@ -15,14 +15,27 @@ with open('test.csv') as inf:
 
 print "headlist: ", headlist
 print "columns : ", partlen
+new_cvs_file_list = [365]
+new_cvs_file_list[0] = "empty"
+
+replace_append_data = ''
 
 for colum in range(partlen):
     print "COLUM NAME: ", headlist[colum]
     check_empty_data = True
+    countlines = 0
     with open('test.csv') as inf:
         for line in inf:
             parts = line.split(",") # split line into parts
             if len(parts) > 1:   # if at least 2 parts/columns
+                if colum == 0:
+                    print "start: count lines = 0"
+                    new_cvs_file_list[colum] = parts[colum]
+                else:
+                    print "countlines: ", countlines
+                    replace_append_data = new_cvs_file_list[colum]
+                    new_cvs_file_list[colum] = replace_append_data + parts[colum]
+
                 if not head:
                     head = parts[colum]
                 else:
@@ -32,6 +45,7 @@ for colum in range(partlen):
             elif parts[colum] != '"0"':
                 print "parts have value: ", parts[colum]
                 check_empty_data = False
+            countlines +=1
     if not check_empty_data:
         print "DATA in: ", headlist[colum]
     else:
